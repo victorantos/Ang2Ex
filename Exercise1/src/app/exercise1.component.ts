9897703e91b804a430afa3b5300b4fb77a57130e
victorantos <github@victorantos.com>
2016-06-29 15:55:56 +0100

    The solution. Added setTimeout function and binded style.color attribute

--- Exercise1/src/app/exercise1.component.ts
+++ Exercise1/src/app/exercise1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: 'exercise1.component.html',
   styleUrls: ['exercise1.component.css']
 })
-export class Exercise1AppComponent {
-  title = 'exercise1 works!';
+export class Exercise1AppComponent implements OnInit {
+  title = 'Exercise 1';
+  items: Array<string> = ['item1', 'item2', 'item3'];
+  activeItem: number = 0;
+
+  ngOnInit():  any {
+   let self = this; 
+    
+    setInterval(function () {
+
+      self.activeItem++;
+      if (self.activeItem == self.items.length)
+        self.activeItem = 0;
+    }, 1000 * 4);
+  }
 }
